e75dc23010cfe5da85a466d9396b699560eed74c
N.D.P.A.Naulla <139682592+anuththara29@users.noreply.github.com>
2025-11-16 22:09:56 +0530

    update producer to produce bad message for demo purpose

--- app/producer.py
+++ app/producer.py
@@ -13,10 +13,19 @@ def load_schema():
     return key_schema, value_schema
 
 def make_order():
+    """Generate VALID order"""
     return {
         "orderId": str(uuid.uuid4()),
         "product": random.choice(["Item1", "Item2", "Item3", "Item4"]),
         "price": round(random.uniform(10, 200), 2)
+    }
+
+def make_bad_order():
+    """Generate INVALID order for DLQ"""
+    return {
+        "orderId": str(uuid.uuid4()),
+        "product": "BAD_ITEM",
+        "price": -50.0        # negative price → permanent error in consumer
     }
 
 def main(n=10):
@@ -26,11 +35,19 @@ def main(n=10):
         default_key_schema=key_schema,
         default_value_schema=value_schema,
     )
+
     for _ in range(n):
-        order = make_order()
+        # 20% probability create a BAD message
+        if random.random() < 0.2:
+            order = make_bad_order()
+            print("Produced BAD ORDER (will go to DLQ):", order)
+        else:
+            order = make_order()
+            print("Produced:", order)
+
         producer.produce(topic=TOPIC_ORDERS, key=order["orderId"], value=order)
-        print("Produced:", order)
         time.sleep(0.5)
+
     producer.flush()
 
 if __name__ == "__main__":
